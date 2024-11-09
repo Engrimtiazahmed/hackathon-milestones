b4ca8c0b4faec0341cb959b008b54e184bfc1b50
Engr Imtiaz Ahmed <160152394+Engrimtiazahmed@users.noreply.github.com>
2024-11-09 12:46:53 +0500

    Add files via upload

---
+++ MileStone-3/script.ts
@@ -0,0 +1,38 @@
+//Get refences to the form and display area
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLFormElement;
+
+//Handle Form Submission
+form.addEventListener('submit', (event: Event) =>{
+event?.preventDefault();
+const name =(document.getElementById('name') as HTMLInputElement).value
+const email =(document.getElementById('email') as HTMLInputElement).value
+const mobile =(document.getElementById('phone') as HTMLInputElement).value
+const education =(document.getElementById('education') as HTMLInputElement).value
+const experience =(document.getElementById('experience') as HTMLInputElement).value
+const skills =(document.getElementById('skills') as HTMLInputElement).value
+
+const resumeHTML = `
+<h2><b>Resume</b></h2>
+<h3>Personal Information</h3>
+<p><b>Name:</b>${name}</p>
+<p><b>Email:</b>${email}</p>
+<p><b>Mobile:</b>${mobile}</p>
+
+<h3>Education</h3>
+<p><b>Education:</b>${education}</p>
+
+<h3>Experience</h3>
+<p><b>Experience:</b>${experience}</p>
+
+<h3>Skills</h3>
+<p><b>Skills:</b>${skills}</p>`;
+
+//DIsplay the generated resume
+if(resumeDisplayElement){
+   resumeDisplayElement.innerHTML = resumeHTML;
+   }else {
+      console.error('The resume display element is missing');
+   }
+
+}); 
